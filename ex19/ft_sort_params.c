7487a48fa5f2f91ab5b70d68b821bcf7956dc1d1
mbjabalquinto <124250038+mbjabalquinto@users.noreply.github.com>
2025-10-30 13:14:33 +0100

    Add files via upload

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,22 +6,56 @@
 /*   By: mjabalqu <mjabalqu@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 15:19:28 by mjabalqu          #+#    #+#             */
-/*   Updated: 2025/10/30 11:20:41 by mjabalqu         ###   ########.fr       */
+/*   Updated: 2025/10/30 13:07:18 by mjabalqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_putchar(char c);
+
+void	ft_print_params(int argc, char ***argv)
 {
-	write(1, &c, 1);
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while ((*argv)[i][j] != '\0')
+		{
+			ft_putchar((*argv)[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+void	ft_sort_params(char **str1, char **str2)
+{
+	int		i;
+	char	*temp;
+
+	i = 0;
+	while ((*str1)[i] != '\0' || (*str2)[i] != '\0')
+	{
+		if ((unsigned char)(*str1)[i] - (unsigned char)(*str2)[i] > 0)
+		{
+			temp = *str1;
+			*str1 = *str2;
+			*str2 = temp;
+			break ;
+		}
+		else if ((unsigned char)(*str1)[i] - (unsigned char)(*str2)[i] < 0)
+			break ;
+		i++;
+	}
 }
 
 int	main(int argc, char *argv[])
 {
 	int	i;
 	int	j;
-	int	c;
-	char	*temp;
 
 	i = 1;
 	if (argc > 1)
@@ -31,34 +65,12 @@ int	main(int argc, char *argv[])
 			j = i + 1;
 			while (j < argc)
 			{
-				c = 0;
-				while (argv[i][c] != '\0' || argv[j][c] != '\0')
-				{
-					if ((unsigned char)argv[i][c] - (unsigned char)argv[j][c] > 0)
-					{
-						temp = argv[i];
-						argv[i] = argv[j];
-						argv[j] = temp;
-						break;
-					}
-					c++;
-				}
+				ft_sort_params(&argv[i], &argv[j]);
 				j++;
 			}
 			i++;
 		}
 	}
-	i = 1;
-	while (i < argc)
-	{
-		j = 0;
-		while (argv[i][j] != '\0')
-		{
-			ft_putchar(argv[i][j]);
-			j++;
-		}
-		ft_putchar('\n');
-		i++;
-	}
+	ft_print_params(argc, &argv);
 	return (0);
 }
