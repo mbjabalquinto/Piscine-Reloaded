58d35d6be6c60813fb1c5a74721b14e926a904a0
mbjabalquinto <124250038+mbjabalquinto@users.noreply.github.com>
2025-10-30 11:35:08 +0100

    Add files via upload

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjabalqu <mjabalqu@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 12:07:01 by mjabalqu          #+#    #+#             */
+/*   Updated: 2025/10/28 13:41:23 by mjabalqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_putchar(char c);
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
+}
