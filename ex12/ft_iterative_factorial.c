67f99d528b83aab4ce236a4d11e60002d929a5f0
mbjabalquinto <124250038+mbjabalquinto@users.noreply.github.com>
2025-10-30 11:35:58 +0100

    Add files via upload

---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjabalqu <mjabalqu@student.42malaga.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 14:34:32 by mjabalqu          #+#    #+#             */
+/*   Updated: 2025/10/29 12:16:50 by mjabalqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb < 0 || nb > 12)
+		return (0);
+	if (nb == 0)
+		return (1);
+	while (nb > 0)
+	{
+		result = result * nb;
+		nb--;
+	}
+	return (result);
+}
